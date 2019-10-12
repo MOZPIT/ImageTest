fecdf4723506d282f1be520cc8da6e6f59664236
Mo Sanchez <mo@vitals.agency>
2019-10-12 07:40:17 -0400

    Added a Mouselook test script to test mouse actions as well as created the GameAction class to track mouse and keyboard input moves

--- src/MouselookTest.java
+++ src/MouselookTest.java
@@ -28,5 +28,116 @@ public class MouselookTest extends GameCore1 implements MouseMotionListener, Key
 		relativeMouseMode = true;
 		isRecentering = false;
 		
+		Cursor invisibleCursor = Toolkit.getDefaultToolkit().createCustomCursor(Toolkit.getDefaultToolkit().getImage(""), new Point(0,0), "invisible");
+		
+		try {
+			robot = new Robot();
+			recenterMouse();
+			mouseLocation.x = centerLocation.x;
+			mouseLocation.y = centerLocation.y;
+		}
+		catch(AWTException ex) {
+			System.out.println("Couldn't create robot!");
+		}
+		Window window = screen.getFullScreenWindow();
+		window.addMouseMotionListener(this);
+		window.addKeyListener(this);
+		window.setCursor(invisibleCursor);
+		bgImage = loadImage("../images/background.jpg");
 	}
+	
+	private Image loadImage(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public synchronized void draw(Graphics2D g) {
+		int w = screen.getWidth();
+		int h = screen.getHeight();
+		
+		//make sure position is correct
+		imageLocation.x %= w; 
+		imageLocation.y %= screen.getHeight();
+		if(imageLocation.x < 0) {
+			imageLocation.x += w;
+		}
+		if(imageLocation.y < 0) {
+			imageLocation.y += screen.getHeight();
+		}
+		
+		//draw the image in four places to cover the screen
+		int x = imageLocation.x;
+		int y = imageLocation.y;
+		g.drawImage(bgImage,  x,  y,  null);
+		g.drawImage(bgImage, x-w, y, null);
+		g.drawImage(bgImage, x, y-h, null);
+		g.drawImage(bgImage, x-w , y-h, null);
+		
+		//draw instructions
+		g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		g.drawString("Press Space to change mouse modes.", 5, FONT_SIZE);
+		g.drawString("Press Escape to exit.", 5, FONT_SIZE*2);
+	}
+	
+	/*
+	 * Uses the robot class to try to position the mouse in the center of the screen. <p>Note that the use of the Robot class may not be available on all platforms.
+	 */
+	private synchronized void recenterMouse() {
+		Window window = screen.getFullScreenWindow();
+		if(robot != null && window.isShowing()) {
+			centerLocation.x = window.getWidth() / 2;
+			centerLocation.y = window.getHeight() / 2;
+			SwingUtilities.convertPointToScreen(centerLocation, window);
+			isRecentering = true;
+			robot.mouseMove(centerLocation.x, centerLocation.y);
+		}
+	}
+	
+	//From the MouseMotionListener interface
+	public void mouseDragged(MouseEvent e) {
+		mouseMoved(e);
+	}
+	
+	//From the MouseMotionListener interface
+	public synchronized void mouseMoved(MouseEvent e) {
+		//this event is from re-centering the mouse - ignore it
+		if(isRecentering && centerLocation.x == e.getX() && centerLocation.y == e.getY()) {
+			isRecentering = false;
+		}else {
+			int dx = e.getX() - mouseLocation.x;
+			int dy = e.getY() - mouseLocation.y;
+			imageLocation.x += dx;
+		    imageLocation.y += dy;
+		    //recenter the mouse
+		    if(relativeMouseMode) {
+		    	recenterMouse();
+		    }
+		}
+		
+		mouseLocation.x = e.getX();
+		mouseLocation.y = e.getY();
+	}
+	
+	//From the KeyListener interface
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+			//exit the program
+			stop();
+		}else if(e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+			//change relative mouse mode
+			relativeMouseMode = !relativeMouseMode;
+		}
+	}
+	
+	//From the KeyListener interface
+	public void keyReleased(KeyEvent e) {
+		//do nothing
+	}
+	
+	//From the KeyListener interface
+	public void keyTyped(KeyEvent e) {
+		//do nothing
+	}
+	
+	
 }
